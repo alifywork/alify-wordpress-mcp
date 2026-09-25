f82274c03e5e63863fee865ac8c39817cd9385a5
Muhammad Humdan Ali <alifywork@gmail.com>
2026-09-26 03:12:41 +0500

    chore: clean v1.5.0 managed structure options on uninstall

--- uninstall.php
+++ uninstall.php
@@ -9,3 +9,6 @@ wp_clear_scheduled_hook( 'wpcmcp_daily_maintenance' );
 delete_option( 'wpcmcp_settings' );
 delete_option( 'wpcmcp_db_version' );
 delete_option( 'wpcmcp_theme_source_backups' );
+delete_option( 'wpcmcp_managed_post_types' );
+delete_option( 'wpcmcp_managed_taxonomies' );
+delete_option( 'wpcmcp_managed_acf_option_pages' );
